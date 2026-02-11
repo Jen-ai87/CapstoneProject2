72c1277b3a5cdd758139343beab1d2ebd3b28ace
Faraz S <63739465+Fraz23@users.noreply.github.com>
2026-02-11 18:17:24 -0500

    build standings and favourites data

---
+++ 90Mins/src/data/favourites.ts
@@ -0,0 +1,28 @@
+import { FavouriteTeam } from './types';
+
+export const favouriteTeams: FavouriteTeam[] = [
+  {
+    teamId: 'man-utd',
+    nextMatch: {
+      opponent: 'Liverpool',
+      date: 'Nov 19',
+      time: '15:00',
+    },
+  },
+  {
+    teamId: 'real-madrid',
+    nextMatch: {
+      opponent: 'Barcelona',
+      date: 'Nov 20',
+      time: '21:00',
+    },
+  },
+  {
+    teamId: 'bayern',
+    nextMatch: {
+      opponent: 'Dortmund',
+      date: 'Nov 19',
+      time: '13:30',
+    },
+  },
+];
